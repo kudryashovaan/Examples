643b3d7fc7f53ad3af98af72dc2ffd6a46a8f817
Alena <kudryashova_a_n@example.com>
2022-06-25 14:14:35 +0300

    009

---
+++ Example009_IntroArrey/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result = arg2;
+    if(arg3>result) result = arg3;
+    return result;
+}
+
+int[] arrey = {11,21,31,41,15,61,17,18,19};
+
+int result = Max(
+    Max(arrey[0],arrey[1],arrey[2]), 
+    Max(arrey[3],arrey[4],arrey[5]),
+    Max(arrey[6],arrey[7],arrey[8]));
+
+Console.WriteLine(result);
