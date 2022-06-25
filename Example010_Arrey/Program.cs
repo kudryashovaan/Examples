9e738bc6275987ad10aacc51267c2964d6a7b26c
Alena <kudryashova_a_n@example.com>
2022-06-25 14:23:48 +0300

    010

---
+++ Example010_Arrey/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arrey = {1,12,31,4,18,16,17,18};
+int n = arrey.Length;
+int find = 18;
+int index = 0;
+
+while(index<n){
+    if(arrey[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
