ccffa0d8324c1fe3db0a0eeabc1d8c3dad0dfb63
Alena <kudryashova_a_n@example.com>
2022-07-02 13:22:49 +0300

    012

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -39,3 +39,10 @@ string res = Method4(10, "asdf");
 Console.WriteLine(res);
 
 
+for(int i = 2; i<=10; i++){
+        for(int j = 2; j<=10; j++){
+            Console.WriteLine($"{i} x {j} = {i*j}");
+        }
+        Console.WriteLine();
+    }
+
